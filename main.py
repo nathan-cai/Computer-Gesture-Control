a65de69e5054e6b252333120aa7f9a625bb9231e
Qien Nathan Cai <qien.nathan@gmail.com>
2022-12-20 03:42:04 -0500

    Add basic hand tracking

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLMS in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLMS)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
