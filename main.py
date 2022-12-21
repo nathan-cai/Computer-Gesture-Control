c18cfdfb03d80d11eb0ed62c22368ea8175bb7df
Qien Nathan Cai <qien.nathan@gmail.com>
2022-12-21 01:49:39 -0500

    add FPS Counter

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
@@ -13,7 +16,14 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLMS in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLMS)
+            mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70),
+                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
