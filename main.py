42b8c4d937d049f91cb06342ec06d195bd2b5ce1
Qien Nathan Cai <qien.nathan@gmail.com>
2023-03-23 13:30:57 -0400

    create hand tracking without classes

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import cv2
 import mediapipe as mp
 import time
+import HandTrackingModule as htm
 
-cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
-pTime = 0
-cTime = 0
 
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = htm.handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
 
-    if results.multi_hand_landmarks:
-        for handLMS in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLMS, mpHands.HAND_CONNECTIONS)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
 
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
-    cv2.putText(img, str(int(fps)), (10, 70),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
